566e00547258caf2676f975e6ab1fde2ac69ab60
Timeless101 <153778193+Timeless101@users.noreply.github.com>
2026-07-22 22:06:30 +0200

    making of test

--- tests/vault_logic/test_start_flow.py
+++ tests/vault_logic/test_start_flow.py
@@ -1,2 +1,13 @@
 import src.vault_logic
 import src.errors
+
+def test_start_flow_happy_test(monkeypatch):
+
+    def fake_get_five_rows_out_database(userid):
+        return ["test", "test", "test", "test", "test"]
+
+    def fake_get_all_items_in_database(userid):
+        return 5
+
+    
+    
