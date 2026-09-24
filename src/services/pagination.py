9aec85fa70284e5cb8bacc70c9733ce00755999c
Timeless101 <153778193+Timeless101@users.noreply.github.com>
2026-09-24 20:10:36 +0200

    Made showing items so they get calculated

--- src/services/pagination.py
+++ src/services/pagination.py
@@ -12,10 +12,18 @@ class Pagination():
         self.pages: int = (self.current_page - 1) * self.page_size
         self.showed_items: int = self.pages + self.page_size
 
-        self.showing_items_start: int = (self.current_page * self.page_size) - 4
-        self.showing_items_end: int = min(self.current_page * self.page_size, total_cred)
+        
+        
 
+    @property
+    def showing_items_start(self) -> int:
+        return (self.current_page * self.page_size) - 4
 
+    @property
+
+    def showing_items_end(self) -> int:
+        return min(self.current_page * self.page_size, self.total_cred)
+        
     def next_page(self):
         if self.current_page < self.total_pages:
             self.current_page += 1
