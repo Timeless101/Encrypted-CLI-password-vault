c9cda04425497743c1bc269eff17507fd5ed076f
Timeless101 <153778193+Timeless101@users.noreply.github.com>
2026-09-24 19:51:26 +0200

    Made the Previous work and next page

--- src/services/pagination.py
+++ src/services/pagination.py
@@ -10,21 +10,21 @@ class Pagination():
 
         self.total_pages: int = math.ceil(total_cred / self.page_size)
         self.pages: int = (self.current_page - 1) * self.page_size
-        self.howed_items: int = self.pages + self.page_size
-        self.showing_items_start: int = (self.current_page * self.page_size) - 4
-        self.showing_items_end: int = min(self.current_page * self.page_size, total_cred)
-        self.pages: int = (self.current_page - 1) * self.page_size
         self.showed_items: int = self.pages + self.page_size
 
+        self.showing_items_start: int = (self.current_page * self.page_size) - 4
+        self.showing_items_end: int = min(self.current_page * self.page_size, total_cred)
+
+
     def next_page(self):
-        if self.showed_items < self.total_cred:
+        if self.current_page < self.total_pages:
             self.current_page += 1
             self.offset += self.page_size
         else:
             pass
 
     def previous_page(self):
-        if self.showed_items > self.page_size:
+        if self.current_page > 1:
             self.current_page -= 1
             self.offset -= self.page_size
         else:
