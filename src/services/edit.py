baedb9910c70387138f2338231b5ff6a220fefe2
Timeless101 <djwuck123@hotmail.com>
2026-09-25 10:03:02 +0200

    Removed the class in edit.

--- src/services/edit.py
+++ src/services/edit.py
@@ -2,68 +2,68 @@ import src.interface.edit_interface as edit_interface
 import src.storage.storage_logic as storage_logic
 import src.crypto as crypto
 
-class Edit_searcher:
 
-    def update_data(self, column: str, cred_id: int, userid: int) -> True:
-        while True:
-            current_data = storage_logic.searcher(
-                columns=[column,],
-                column=("cred_id",),
-                data_to_search=cred_id,
-                userid=userid
-            )[0][0]
 
-            new_data = edit_interface.Edit_prompt.new_data_question(column, current_data)
-            awnser = edit_interface.Edit_prompt.confirmation()
+def update_data(column: str, cred_id: int, userid: int) -> True:
+    while True:
+        current_data = storage_logic.searcher(
+            columns=[column,],
+            column=("cred_id",),
+            data_to_search=cred_id,
+            userid=userid
+        )[0][0]
 
-            if awnser.lower() not in ("y", "yes"):
-                continue
-            break
-                
+        new_data = edit_interface.Edit_prompt.new_data_question(column, current_data)
+        awnser = edit_interface.Edit_prompt.confirmation()
 
-        self.search = storage_logic.update_database_item(
-            cred_id=cred_id,
-            userid=userid,
-            column=column,
-            new_data=new_data
-        )
+        if awnser.lower() not in ("y", "yes"):
+            continue
+        break
+            
 
-        return True
+    storage_logic.update_database_item(
+        cred_id=cred_id,
+        userid=userid,
+        column=column,
+        new_data=new_data
+    )
 
-    def update_password(self, column: str, cred_id: int, userid: int, encryption_key: bytes) -> True:
-        while True:
-                    password_encrypted = storage_logic.searcher(
-                        columns=[column,],
-                        column=("cred_id",),
-                        data_to_search=cred_id,
-                        userid=userid
-                    )[0][0]
+    return True
 
-                    current_data = crypto.password_decryption(
-                        password=password_encrypted,
-                        encryption_key=encryption_key
-                    )
+def update_password(column: str, cred_id: int, userid: int, encryption_key: bytes) -> True:
+    while True:
+                password_encrypted = storage_logic.searcher(
+                    columns=[column,],
+                    column=("cred_id",),
+                    data_to_search=cred_id,
+                    userid=userid
+                )[0][0]
 
-                    password_decrypted = edit_interface.Edit_prompt.new_data_question(column, current_data)
-                    awnser = edit_interface.Edit_prompt.confirmation()
-        
-                    if awnser.lower() not in ("y", "yes"):
-                        continue
-                    break
+                current_data = crypto.password_decryption(
+                    password=password_encrypted,
+                    encryption_key=encryption_key
+                )
 
-        password_encrypted = crypto.password_encryption(
-            password=password_decrypted,
-            encryption_key=encryption_key
-        )
-        
-        self.search = storage_logic.update_database_item(
-            cred_id=cred_id,
-            userid=userid,
-            column=column,
-            new_data=password_encrypted
-        )
+                password_decrypted = edit_interface.Edit_prompt.new_data_question(column, current_data)
+                awnser = edit_interface.Edit_prompt.confirmation()
+    
+                if awnser.lower() not in ("y", "yes"):
+                    continue
+                break
 
-        return True
+    password_encrypted = crypto.password_encryption(
+        password=password_decrypted,
+        encryption_key=encryption_key
+    )
+    
+    storage_logic.update_database_item(
+        cred_id=cred_id,
+        userid=userid,
+        column=column,
+        new_data=password_encrypted
+    )
+
+    return True
 
 def main(cred_id: int, userid: int, encryption_key: bytes) -> None:
 
@@ -82,25 +82,25 @@ def main(cred_id: int, userid: int, encryption_key: bytes) -> None:
         )
 
 def choice_table(choice, cred_id: int, userid: int, encryption_key: bytes) -> None:
-    update = Edit_searcher()
+
     match choice:
 
         case "1":
-            update.update_data(
+            update_data(
                 column="Service",
                 cred_id=cred_id,
                 userid=userid,
             )
 
         case "2":
-            update.update_data(
+            update_data(
                 column="Username",
                 cred_id=cred_id,
                 userid=userid,
             )
 
         case "3":
-            update.update_password(
+            update_password(
                 column="Password",
                 cred_id=cred_id,
                 userid=userid,
@@ -108,7 +108,7 @@ def choice_table(choice, cred_id: int, userid: int, encryption_key: bytes) -> No
             )
 
         case "4":
-            update.update_data(
+            update_data(
                 column="Comment",
                 cred_id=cred_id,
                 userid=userid,
